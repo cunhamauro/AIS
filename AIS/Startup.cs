2b250a70357effbd100b3983a64dd266cfa7b2ab
Mauro Cunha <cunhamauro@outlook.pt>
2024-10-09 09:28:26 +0100

    default error routing correction

--- AIS/Startup.cs
+++ AIS/Startup.cs
@@ -134,7 +134,7 @@ namespace AIS
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Shared/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
